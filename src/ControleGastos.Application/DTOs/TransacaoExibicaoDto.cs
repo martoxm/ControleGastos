7164a3207f08515e5e1505415a307abba38faac1
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-07-03 20:17:57 -0300

    docs: ajusta comentario do dto de exibicao de transacao

--- src/ControleGastos.Application/DTOs/TransacaoExibicaoDto.cs
+++ src/ControleGastos.Application/DTOs/TransacaoExibicaoDto.cs
@@ -1,8 +1,8 @@
 ﻿namespace ControleGastos.Application.DTOs
 {
     /// <summary>
-    /// DTO focado no retorno visual de dados de transações.
-    /// Converte o Enum interno para string amigável para o Frontend.
+    /// DTO focado no retorno de dados de transações para exibição.
+    /// Converte o enum interno para uma string mais amigável ao frontend.
     /// </summary>
     public class TransacaoExibicaoDto
     {
