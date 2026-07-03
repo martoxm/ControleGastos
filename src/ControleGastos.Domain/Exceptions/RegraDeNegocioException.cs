64ac3684aa6654c8310e4aebe165b493913ec661
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-07-03 20:50:41 -0300

    feat: adiciona excecao de regra de negocio no dominio

---
+++ src/ControleGastos.Domain/Exceptions/RegraDeNegocioException.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleGastos.Domain.Exceptions
+{
+    /// <summary>
+    /// Exceção usada quando uma regra de negócio do domínio é violada.
+    /// Esse tipo de erro representa uma operação inválida do ponto de vista do sistema,
+    /// mas não necessariamente uma falha interna da aplicação.
+    /// </summary>
+    public class RegraDeNegocioException(string message) : Exception(message)
+    {
+    }
+}
